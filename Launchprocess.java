8a43c4137838257350dbee2372d5295759b5cd1d
MichaelLaytonOliver <159829406+MichaelLaytonOliver@users.noreply.github.com>
2025-02-12 09:45:08 -0500

    heres a new timer

--- Launchprocess.java
+++ Launchprocess.java
@@ -1,3 +1,38 @@
+
+
+public class Launchprocess extends Main  {
+   
+    
+   public static int i;
+    public static void main(String[] args) {
+   Astronaut a = new Astronaut();
+
+   for (int i = 10; i >= 0; i--) {
+    System.out.println(i);
+    try {
+        Thread.sleep(1000); // Delay of 1 second
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+}
+System.out.println("Blast off"); 
+
+
+}
+
+}
+   
+
+        
+    
+
+
+
+
+    
+    
+
+
  
 
 
